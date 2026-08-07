7bfb32df25b6e1d8b6d3f302b651c2ee29fe7d1f
lestrrat <49281+lestrrat@users.noreply.github.com>
2026-08-07 13:29:37 +0900

    generate taskboard schema from migrations (#76)

--- sample/taskboard/internal/store/generate.go
+++ sample/taskboard/internal/store/generate.go
@@ -1,3 +1,4 @@
-//go:generate go run github.com/lestrrat-go/rasql/cmd/rasqlgen schema -input schema.json -package store -output schema_gen.go
+//go:generate go -C ../../../.. run ./cmd/rasqlmigrate apply -dir sample/taskboard/migrations/sqlite -dialect sqlite -dsn sample/taskboard/internal/store/.taskboard-schema.db
+//go:generate go -C ../../../.. run ./cmd/rasqlgen schema -dsn sample/taskboard/internal/store/.taskboard-schema.db -dialect sqlite -table members -table projects -table tasks -package store -output sample/taskboard/internal/store/schema_gen.go
 
 package store
