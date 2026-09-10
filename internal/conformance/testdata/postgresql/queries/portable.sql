eff0380d52d671ceca2db31bb2ba38a7de1590e8
Daisuke Maki <49281+lestrrat@users.noreply.github.com>
2026-09-10 13:53:43 +0900

    regenerate conformance fixtures from a database

--- internal/conformance/testdata/postgresql/queries/portable.sql
+++
@@ -1,2 +0,0 @@
-SELECT id, name FROM members WHERE id = ? ORDER BY id;
-SELECT p.id, t.id, m.name FROM projects p LEFT JOIN tasks t ON t.project_id = p.id LEFT JOIN members m ON m.id = t.assignee_id ORDER BY p.id, t.id;
